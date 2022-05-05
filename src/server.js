74695cc4eb0b4ee0def5182b5e11658c84f1081e
InvertedOwl <bach0770>
2022-05-05 08:24:07 -0600

    Fixed grammar issues

--- src/server.js
+++ src/server.js
@@ -9,17 +9,17 @@ Database.addRowToDatabase({name: "Shane Andrus", description: "I made this in 30
 const server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });   
-        res.write('<html><body><p>This is home Page.</p><a href="/bhg">🔇 7️⃣ 🛍 📴 😹</a></body></html>');
+        res.write('<html><body><p>This is home page.</p><a href="/bhg">Mystey link!</a></body></html>');
         res.end();
     }
     else if (req.url == "/student") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p><h1>wes was here</h1></body></html>');
+        res.write('<html><body><p>This is student page.</p><p>Wesley has been here.</p></body></html>');
         res.end();
     }
     else if (req.url == "/admin") {   
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.write('<html><body><p>This is admin page.</p></body></html>');
         res.end();
     }
     else if (req.url == "/bhg") { // bhg is brohousegames which is my website (https://brohouse.dev)
